b379fbf024399978951f62b15709f3a483fd5152
defan312 <defansugiarto5@gmail.com>
2024-05-08 15:37:34 +0700

    Upload di Github

---
+++ plugins/donasi.js
@@ -0,0 +1,31 @@
+import fetch from 'node-fetch'
+let handler = async (m, { conn, command, usedPrefix }) => {
+let str = `${conn.getName(m.sender)}
+Want Support Bot?
+
+*[ PAYMENT METHOD ]*
+
+- Pulsa/pulse(Telkomseli): *081321776496*
+- Dana/Gopay/Ovo: *081321776496*
+
+Setelah melakukan donasi kirim bukti pembayaran ke owner
+`
+await conn.relayMessage(m.chat,  {
+    requestPaymentMessage: {
+      currencyCodeIso4217: 'JPY',
+      amount1000: '1000',
+      requestFrom: '0@s.whatsapp.net',
+      noteMessage: {
+      extendedTextMessage: {
+      text: str,
+      contextInfo: {
+      mentionedJid: [m.sender],
+      externalAdReply: {
+      showAdAttribution: true
+      }}}}}}, {})
+}
+handler.help = ['donasi']
+handler.tags = ['info']
+handler.command = /^dona(te|si)$/i
+
+export default handler
